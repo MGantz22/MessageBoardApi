c78b7e84b12f8066374516de9b48486c51a28bae
Mitchell Gantz <mgantz22@icloud.com>
2023-03-29 10:11:55 -0700

    Add Identity

--- Startup.cs
+++ Startup.cs
@@ -13,6 +13,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using MessageBoard.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace MessageBoard
 {
